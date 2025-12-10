1a0bad6283449a60b4faa90d1d161ad09704978a
JuanCROliveira <juan_btu@hotmail.com>
2025-12-10 11:47:57 -0300

    padel.sh

--- padel.sh
+++ padel.sh
@@ -3,11 +3,11 @@
 java -Xms1G -Xmx2G -jar PaDEL-Descriptor.jar \
   -dir input/ \
   -file output.csv \
-  -2d \
   -fingerprints \
   -retainorder \
   -removesalt \
   -standardizenitro \ 
   -threads 2
+
 
 
